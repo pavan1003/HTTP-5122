fc1d21ca11b3114534258f41a12aeaea1f755f53
Pavan Mistry <pavan32100@gmail.com>
2024-01-31 09:47:40 -0500

    Added in-class lab 4

---
+++ InClassLabW4/JS-LAB-4-Functions-24w/lab-4-2.js
@@ -0,0 +1,37 @@
+﻿//#### LAB 4 - FUNCTIONS ####
+//PART 2:  AN AVERAGE FUNCTION
+
+//################## CREATE YOUR AVERAGE FUNCTION
+//This function takes five numbers and returns their average to one decimal place.
+function averageOfFiveNumbers(num1, num2, num3, num4, num5) {
+  var average = ((num1 + num2 + num3 + num4 + num5) / 5).toFixed(1);
+  return average;
+}
+console.log(averageOfFiveNumbers(5, 10, 15, 20, 25));
+
+//################## LOGIC THAT OUTPUTS MESSAGES BASED ON FUNCTION RESULTS
+// var HTTP5110 = 99;
+// var HTTP5114 = 89;
+// var HTTP5121 = 98;
+// var HTTP5122 = 88;
+// var HTTP5125 = 100;
+
+// var HTTP5110 = 69;
+// var HTTP5114 = 69;
+// var HTTP5121 = 69;
+// var HTTP5122 = 69;
+// var HTTP5125 = 69;
+
+var HTTP5110 = 70;
+var HTTP5114 = 70;
+var HTTP5121 = 70;
+var HTTP5122 = 70;
+var HTTP5125 = 70;
+
+var overallAverage = averageOfFiveNumbers(HTTP5110, HTTP5114, HTTP5121, HTTP5122, HTTP5125);
+
+if (overallAverage >= 70) {
+  alert("Success. Congratulations and keep up the good work!");
+} else {
+  alert("Review required");
+}
---
+++ InClassLabW4/JS-LAB-4-Functions-24w/lab-4-3.js
@@ -0,0 +1,21 @@
+﻿//#### LAB 4 - FUNCTIONS ####
+//PART 3:  WALKING THE DOG
+
+//################## CREATE YOUR checkTemp FUNCTION
+//This function will checks if it is safe or not to walk the dog based on the temperature input.
+//It expects to receive the current temperature as a parameter (variable name: currentTemp, data type: number).
+//It will return a boolean value - true if it's safe to walk, false otherwise.
+function checkTemp(currentTemp) {
+  if (currentTemp > 30 || currentTemp < -10) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+//################## LOGIC THAT OUTPUTS MESSAGES BASED ON FUNCTION RESULTS
+var walkDog = checkTemp(prompt("Whats the current Temperature outside?"));
+
+walkDog
+  ? alert("You're good, have a nice walk!")
+  : alert("Yikes! This is no weather for dog walking!");
