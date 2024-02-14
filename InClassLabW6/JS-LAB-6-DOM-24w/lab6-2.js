2f034f1ed3586a48a5e13d94668c85839bd97c55
Pavan Mistry <pavan32100@gmail.com>
2024-02-14 08:54:24 -0500

    Added assignment 3 and lab 6

---
+++ InClassLabW6/JS-LAB-6-DOM-24w/lab6-2.js
@@ -0,0 +1,35 @@
+﻿//#### LAB 6 - DOM MANIPULATION ####
+//PART 2: MYSTERY BOX
+
+//LISTEN FOR load EVENT
+window.onload = onPageLoad;
+
+//'WRAPPER' FUNCTION FOR DOM LOGIC
+function onPageLoad(){
+
+	//GET DOM ELEMENTS WE'LL NEED
+	var mysteryBox = document.getElementById("mysteryBox");
+	var buttonBox = document.getElementById("buttonBox");
+	
+	//====CREATE THE FUNCTIONS WE'LL NEED====
+	//FUNCTION TO ASK USER
+	function askUser(){
+		var onOk = confirm("Do you want to see something?");
+		if(onOk) {
+			mysteryBox.style.display = "none";
+			buttonBox.style.display = "block";
+		}
+	}
+
+	//FUNCTION TO CHANGE buttonBox
+	function handleBoxClick() {
+		buttonBox.style.width = "600px";
+		buttonBox.innerHTML = "SURPRISE!!";
+		buttonBox.style.backgroundColor = "orange";
+	}
+	
+	//SETUP LISTENERS
+	mysteryBox.onmouseover = askUser;
+	buttonBox.onclick = handleBoxClick;
+
+}//END onload FUNCTION
---
+++ JS_Assignment-3-Objects-24/JS_Assign-3-objects.js
@@ -0,0 +1,27 @@
+﻿// object with 4 properties and 1 function representing a movie of my personal interest.
+var movie = {
+  title: "3 Idiots",
+  director: "Rajkumar Hirani",
+  imdbRating: "8.4/10",
+  genre: "comedy",
+  // Function to modify title value
+  updateTitle: function (newTitle) {
+    movie.title = newTitle;
+    alert("Title of the movie updated to: " + movie.title);
+  }
+};
+
+// object is output to the console before the modifications.
+console.log("Original movie object:", movie);
+
+// Prompt the user for input to update genre property of movie object
+movie.genre = prompt("Enter the new genre for the movie:", movie.genre);
+
+// Prompt the user for input to update director property of movie object
+movie.director = prompt("Enter the new director for the movie:", movie.director);
+
+// Call the object's method to update a property
+movie.updateTitle("3 Idiots - Bollywood");
+
+// object is output to the console after the modifications.
+console.log("Modified movie object:", movie);
