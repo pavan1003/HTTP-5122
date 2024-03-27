86924baad32ed176818debd8a74953bb4e94e78f
Pavan Mistry <pavan32100@gmail.com>
2024-03-27 08:16:35 -0400

    Added Lab10

---
+++ InClassLabW11/JS-LAB-10-jQUERY-24w/lab10-1-jquery.js
@@ -0,0 +1,23 @@
+//LAB 10 - 1 FAQ PAGE
+
+//Listen for window load the jQuery way
+
+
+
+	//Inside of here is your jQuery/JavaScript
+
+
+	//ADD CLICK EVENT TO <h2>
+	
+	
+	
+	
+	
+	
+	//CHANGE <p> BACKGROUND ON HOVER
+
+
+
+
+
+
