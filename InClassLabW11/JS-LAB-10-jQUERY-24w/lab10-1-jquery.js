84d48c0096d7ce8ebf6ec9047d9781d55f279476
Pavan Mistry <pavan32100@gmail.com>
2024-03-27 09:52:58 -0400

    completed lab 10

--- InClassLabW11/JS-LAB-10-jQUERY-24w/lab10-1-jquery.js
+++ InClassLabW11/JS-LAB-10-jQUERY-24w/lab10-1-jquery.js
@@ -1,23 +1,26 @@
 //LAB 10 - 1 FAQ PAGE
 
 //Listen for window load the jQuery way
+jQuery(window).on("load", function () {
+  //Inside of here is your jQuery/JavaScript
+  jQuery("p").css("display", "none");
 
-
-
-	//Inside of here is your jQuery/JavaScript
-
-
-	//ADD CLICK EVENT TO <h2>
-	
-	
-	
-	
-	
-	
-	//CHANGE <p> BACKGROUND ON HOVER
-
-
-
-
-
-
+  function toggleParagraph(thisClass, contentClass) {
+    $(".contentBox").not($(thisClass).next(contentClass)).slideUp(3000);
+    $(thisClass).next(contentClass).slideToggle(3000);
+  }
+  //ADD CLICK EVENT TO <h2>
+  $("#h21").on("click", function () {toggleParagraph(this, ".contentBox");});
+  $("#h22").on("click", function () {toggleParagraph(this, ".contentBox");});
+  $("#h23").on("click", function () {toggleParagraph(this, ".contentBox");});
+  $("#h24").on("click", function () {toggleParagraph(this, ".contentBox");});
+  //CHANGE <p> BACKGROUND ON HOVER
+  $(".contentBox").hover(
+    function () {
+      $(".contentBox").toggleClass("textHovered");
+    },
+    function () {
+      $(".contentBox").toggleClass("textHovered");
+    }
+  );
+});
--- InClassLabW11/JS-LAB-10-jQUERY-24w/lab10-2-jquery.js
+++ InClassLabW11/JS-LAB-10-jQUERY-24w/lab10-2-jquery.js
@@ -1,18 +1,23 @@
 //LAB 10 - 2 INVENTORY PAGE
 
-
-
-
-
-
-
+//Listen for window load the jQuery way
+jQuery(window).on("load", function () {
+	//Inside of here is your jQuery/JavaScript
+	jQuery(".desc").hide();
 
 	//ADD MOUSEOVER/MOUSEOUT FUNCTIONS FOR <tr>
-
-
-
-
-
-
+	$("tr").hover(
+		function () {
+			$(this).toggleClass("selected");
+		},
+		function () {
+			$(this).toggleClass("selected");
+		}
+	);
 
 	//ADD CLICK EVENT TO <tr>
+	$("tr").on("click", function () {
+		$(".desc").hide();
+		$(this).find(".desc").show();
+	});
+});
