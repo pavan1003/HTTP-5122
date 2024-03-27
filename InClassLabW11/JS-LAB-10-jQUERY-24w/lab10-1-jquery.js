889280edef1f53656bc696d32aacfd48b4daab16
Pavan Mistry <pavan32100@gmail.com>
2024-03-27 09:55:43 -0400

    minor updates

--- InClassLabW11/JS-LAB-10-jQUERY-24w/lab10-1-jquery.js
+++ InClassLabW11/JS-LAB-10-jQUERY-24w/lab10-1-jquery.js
@@ -3,17 +3,17 @@
 //Listen for window load the jQuery way
 jQuery(window).on("load", function () {
   //Inside of here is your jQuery/JavaScript
-  jQuery("p").css("display", "none");
+  jQuery("p").hide();
 
   function toggleParagraph(thisClass, contentClass) {
     $(".contentBox").not($(thisClass).next(contentClass)).slideUp(3000);
     $(thisClass).next(contentClass).slideToggle(3000);
   }
   //ADD CLICK EVENT TO <h2>
-  $("#h21").on("click", function () {toggleParagraph(this, ".contentBox");});
-  $("#h22").on("click", function () {toggleParagraph(this, ".contentBox");});
-  $("#h23").on("click", function () {toggleParagraph(this, ".contentBox");});
-  $("#h24").on("click", function () {toggleParagraph(this, ".contentBox");});
+  $("#h21").on("click", function () { toggleParagraph(this, ".contentBox"); });
+  $("#h22").on("click", function () { toggleParagraph(this, ".contentBox"); });
+  $("#h23").on("click", function () { toggleParagraph(this, ".contentBox"); });
+  $("#h24").on("click", function () { toggleParagraph(this, ".contentBox"); });
   //CHANGE <p> BACKGROUND ON HOVER
   $(".contentBox").hover(
     function () {
